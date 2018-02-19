abf95fa1ce53a9179822c92a7e2112693f1d983b
Gerk <vgerk95@gmail.com>
2018-02-19 21:07:11 +0300

    Adding AppConfig (include web and data conf)

--- src/main/java/by/home/spring/config/AppConfig.java
+++ src/main/java/by/home/spring/config/AppConfig.java
@@ -1,93 +1,9 @@
 package by.home.spring.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableTransactionManagement
-@PropertySource("classpath:database.properties")
-@ComponentScan({"by.home.spring.service"})
-@EnableJpaRepositories("by.home.spring.repository")
+@Import({DataConfig.class, WebConfig.class})
 public class AppConfig {
-
-    @Value("${jdbc.driverClassName}")
-    private String driverClassName;
-
-    @Value("${jdbc.url}")
-    private String url;
-
-    @Value("${jdbc.username}")
-    private String username;
-
-    @Value("${jdbc.password}")
-    private String password;
-
-    @Value("${db.hibernate.dialect}")
-    private String hibernateDialect;
-
-    @Value("${db.hibernate.show_sql}")
-    private Boolean hibernateShowSql;
-
-    @Value("${db.hibernate.hbm2ddl.auto}")
-    private String hibernateHBM2DD1auto;
-
-    @Value("${db.hibernate.schema}")
-    private String hibernateSchema;
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan("by.home.spring.entity");
-        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
-        entityManagerFactoryBean.setJpaProperties(jpaProperties());
-        return entityManagerFactoryBean;
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        return new HibernateJpaVendorAdapter();
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
-
-    private Properties jpaProperties() {
-        Properties properties = new Properties();
-        properties.put("db.hibernate.dialect", hibernateDialect);
-        properties.put("db.hibernate.show_sql", hibernateShowSql);
-        properties.put("db.hibernate.hbm2ddl.auto", hibernateHBM2DD1auto);
-        properties.put("db.hibernate.schema", hibernateSchema);
-        return properties;
-    }
-
 }
